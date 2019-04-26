f147ae03d1b62893843e84a0810ea583a4229c99
japsuchiha <jpntsingh8@gmail.com>
2019-04-26 14:17:32 -0400

    part1 and part2

--- assignments/9_Crypto_I/writeup/crack.py
+++ assignments/9_Crypto_I/writeup/crack.py
@@ -4,13 +4,22 @@ import hashlib
 import string
 
 def crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    hashes = open("hashes.txt") # open and read hashes.txt
+    passwords = open("passwords.txt") # open and read passwords.txt
     characters = string.ascii_lowercase
 
-    for c in characters:
-        for p in passwords:
+    hash = hashes.read().split('\n')
+    # print(hash)
+    for p in passwords:
+        for c in characters:
             # crack hashes
+            x = hashlib.sha256()
+            # print(x)
+            x.update(bytes((c+p).rstrip(),'ascii'))
+
+            if x.hexdigest() in hash:
+                print("{}:{}".format((c+p).rstrip(),x.hexdigest()))
+            
 
             # print hashes as 'input:hash'
             # i.e.  yeet:909104cdb5b06af2606ed4a197b07d09d5ef9a4aad97780c2fe48053bce2be52
--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -1,22 +1,47 @@
-#!/usr/bin/env python3
 
 import hashlib
 import string
 import socket
 import time
+import re
+import pdb
+import select
+
 
 def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    hashes = [line.strip() for line in open("hashes.txt", "r")] # open and read hashes.txt
+    passwords = [line.strip() for line in open("passwords.txt", "r")] # open and read passwords.txt
     characters = string.ascii_lowercase
-    server_ip = 'put_your_ip_here'
-    server_port = 00000
+    server_ip = '134.209.128.58'
+    server_port = 1337
+
+    hash = {}
+
+    for c in characters:
+        for p in passwords:
+            full = (c + p).encode()
+            res = hashlib.sha256(full).hexdigest()
+            hash[res] = full
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
-    data = s.recv(1024)
-    # parse data
+    count = 0
+
     # crack 3 times
+    while (count < 3):
+        s.setblocking(0)
+        ready = select.select([s], [], [], 0.5)
+        if ready[0]:
+            data = s.recv(1024)
+            ans = data.decode().split('\n')[2]
+            print(ans)
+            print(hash[ans])
+            s.send(hash[ans])
+            time.sleep(0.3)
+            count += 1
+
+    data = s.recv(1024)
+    print("flag: {}".format(data))
 
 if __name__ == "__main__":
     server_crack()
